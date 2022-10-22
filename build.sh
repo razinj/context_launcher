2c86985b01cd1c6bf57cd00a5455c490ccbba6a7
razinj <nizarjailane@gmail.com>
2022-10-23 00:23:24 +0100

    Minify release build and add build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd ./android
+
+echo "[!] Current directory $(pwd)"
+
+echo "[!] Started building release APK and AAB binaries"
+# ./gradlew assembleRelease && ./gradlew bundleRelease
+echo "[!] Completed building binaries"
+
+echo "[!] APK location: $(pwd)/app/build/outputs/apk/release"
+ls -l app/build/outputs/apk/release
+echo "[!] AAB location: $(pwd)/app/build/outputs/bundle/release"
+ls -l app/build/outputs/bundle/release
